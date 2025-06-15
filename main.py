121175e0f300fe19f21cf5e6da0c64f241610adb
lavelius <jonathansirmans29@gmail.com>
2025-06-14 17:29:00 -0700

    text is now being sorted into a list but isnt printing in the correct format yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import get_letters
+from stats import sort_letters_dict
 
 #this function read the book text from a file and returns it as a string.
 def get_book_text(file_path):
@@ -12,8 +13,9 @@ def main():
     text = get_book_text("books/frankenstein.txt")
     amount = get_num_words(text)
     letters = get_letters(text)
+    sorted_letters = sort_letters_dict(letters)
     print(f"{amount} words found in the document")
-    print(f"Letters found in the document: {letters}")
+    print(f"Letters found in the document: {sorted_letters}")
     
 
 
--- stats.py
+++ stats.py
@@ -10,3 +10,20 @@ def get_letters(book_text):
             letters[char] = 0
         letters[char] += 1
     return letters
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_letters_dict(letters):
+    sorted_letters = []
+    for char in letters:
+        if char.isalpha():
+            sorted_letters.append({"char" : char, "num" : letters[char]})
+    sorted_letters.sort(key=sort_on, reverse=True)
+    return sorted_letters
+
+
+
+        
+        
+
