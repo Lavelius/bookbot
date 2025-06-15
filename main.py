9d8a88c72f600dd4027e13c17fa26fdb39505ece
lavelius <jonathansirmans29@gmail.com>
2025-06-14 17:46:40 -0700

    project finished! it now works for any books within your books directory

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_num_words
 from stats import get_letters
 from stats import sort_letters_dict
+import sys
 
 #this function read the book text from a file and returns it as a string.
 def get_book_text(file_path):
@@ -10,7 +11,12 @@ def get_book_text(file_path):
     
     #This function runs the program that should count the number of words and letters in a book.
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    try:
+        file_path = sys.argv[1]
+    except IndexError:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    text = get_book_text(file_path)
     amount = get_num_words(text)
     letters = get_letters(text)
     sorted_letters = sort_letters_dict(letters)
